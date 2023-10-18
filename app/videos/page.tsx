8853c28dda10f662b1a0d64a2a5271f1c26d112b
ABHINAV TV <134499697+abhinav6028@users.noreply.github.com>
2023-10-18 08:47:06 +0000

    fix

--- app/videos/page.tsx
+++ app/videos/page.tsx
@@ -21,7 +21,7 @@ export default function page() {
             btnName: 'Beginner'
         },
         {
-            btnName: 'intermediate'
+            btnName: 'Intermediate'
         },
         {
             btnName: 'Expert'
